18d1e41207af22e4e67e802b60d52587f77f94d5
kimyoungsu01 <kysmb200@gmail.com>
2025-08-18 18:10:16 +0900

    [feat] 스크립트 수정

---
+++ Assets/Scripts/UI/MainSceneUI/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    public void OnStageSelector(int sceneNum)
+    {
+        //GameManager.LoadScene(1);
+    }
+
+    public void OnReLoder()
+    {
+        //int currentSceneNum = GameManager.GetActiveSceneNum();
+        //GameManager.LoadScene(currentSceneNum);
+    }
+
+}
