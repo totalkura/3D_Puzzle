4a99ff1424c09e05c640351e043c3e91459c50b5
kimyoungsu01 <kysmb200@gmail.com>
2025-08-18 17:18:59 +0900

    [fix] MainScene 스크립트 수정

---
+++ Assets/Scripts/UI/MainSceneUI/MainScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScene : MonoBehaviour
+{
+
+    public void OnStageSelector(int sceneNum)
+    {
+        MapManager.LoadScene(1);
+    }
+
+    public void OnReLoder()
+    {
+        int currentSceneNum = MapManager.GetActiveSceneNum();
+        MapManager.LoadScene(currentSceneNum);
+    }
+
+}
