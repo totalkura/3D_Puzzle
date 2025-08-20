2a73002ae68b47aecd6a5865f31fda071508fa8b
kimyoungsu01 <kysmb200@gmail.com>
2025-08-20 22:04:30 +0900

    [fix] 체력바 및 스테미너바 확인바 추가

--- Assets/Scripts/UI/MainSceneUI/MapClickController.cs
+++ Assets/Scripts/UI/MainSceneUI/MapClickController.cs
@@ -12,14 +12,18 @@ public class MapClickController : MonoBehaviour
 
     public void Start()
     {
-        MapValueChanged(Vector2.zero);
+        //MapValueChanged(Vector2.zero);
     }
 
     public void MapValueChanged(Vector2 value)
     {
-        for (int i = 0; i < myScrollRect.content.childCount; i++)
+        for (int i = 0; i < myScrollRect.content.childCount; i++) //childCount = 9
         {
+            Vector2 posiotion = myScrollRect.content.GetChild(i).position; // Stages의 값은 W Data : -1550, H Data : 110
+            Vector2 viewCenter = myScrollRect.viewport.position; // viewport의 값은 바텀 : 17
 
+            float distance = Vector2.Distance(posiotion, viewCenter); // 거리 계산
+            //float scale = 
         }
     }
 }
